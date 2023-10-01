f316d785219dc99f1098685e75bfb52eaf96e9ce
jbubu97 <jabbar.babu81@gmail.com>
2023-10-01 18:51:50 +0600

    ath home work

---
+++ math.js
@@ -0,0 +1,31 @@
+// some mathematical operations
+
+let numOne = 10;
+let numTwo = 20;
+
+let addition = numOne + numTwo;
+let subtraction = numOne - numTwo;
+let multification = numOne * numTwo;
+let divition = numOne / numTwo;
+
+// Increment and Decrement
+
+let incementNum = 10;
+incementNum ++; // it will add 1 with previous value
+incementNum += 5; // it will add 51 with previous value
+
+let decrementNum = 10;
+decrementNum --; // it will minus  1 from previous value
+decrementNum -=5; // it will minus  5 from previous value
+// console.log(incementNum);
+
+// comparison
+
+let comparisonZero = numOne ==  numTwo;
+let comparisonOne = numOne !==  numTwo;
+let comparisonTwo = numOne >  numTwo;
+let comparisonThree = numOne < numTwo;
+let comparisonFour = numOne >= numTwo;
+let comparisonFive = numOne <= numTwo;
+
+console.log(comparisonThree);
